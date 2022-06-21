bb6c47839e4815b463865421a3768609e9ddcbea
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-21 12:22:58 +0500

    feat: add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './Header.module.css';
+import logo from '../../image/header/logo.png';
 
 function Header() {
   return (
     <header className={s.header}>
-      
+      <img className={s.header__logo} src={logo} alt="Лого"/>
     </header>
   );
 }
