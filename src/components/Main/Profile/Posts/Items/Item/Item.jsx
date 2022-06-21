47d0e228e804a975010c78e6a2962c2aa4360d62
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-21 19:20:17 +0500

    feat: add props

--- src/components/Main/Profile/Posts/Items/Item/Item.jsx
+++ src/components/Main/Profile/Posts/Items/Item/Item.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import s from './Item.module.css';
 
-function Item() {
+function Item({ text, likes }) {
   return (
     <li className={s.item}>
       <img className={s.avatar} src="https://upload.wikimedia.org/wikipedia/commons/9/93/Amateur-made_Na%27vi.jpg" alt="avatar"/>
-      <p className={s.text}>Hello, world!</p>
+      <p className={s.text}>{text}</p>
       <div className={s.like}>
         <button className={s.heart}></button>
-        <span className={s.counter}>14</span>
+        <span className={s.counter}>{likes}</span>
       </div>
     </li>
   );
--- src/components/Main/Profile/Posts/Items/Items.jsx
+++ src/components/Main/Profile/Posts/Items/Items.jsx
@@ -5,15 +5,10 @@ import Item from './Item/Item';
 function Items() {
   return (
     <ul className={s.items}>
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+      <Item text="It was a good day!" likes="3"/>
+      <Item text="What's happening?" likes="6"/>
+      <Item text="Yeeeaaaaaaaaaaa" likes="11"/>
+      <Item text="Hello, world!" likes="14"/>
     </ul>
   );
 }
