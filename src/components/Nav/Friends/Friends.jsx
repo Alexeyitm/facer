b195141e92b9d07c950812c2094d690d3dd29d63
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-22 17:54:11 +0500

    fix: fixed size nav

---
+++ src/components/Nav/Friends/Friends.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Friends.module.css';
+
+function Friends() {
+  return (
+    <div className={s.friends}>
+
+    </div>
+  );
+}
+
+export default Friends;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Nav.module.css';
+import Friends from './Friends/Friends';
 
 function Nav() {
   return (
-    <nav className={s.nav}>
-      <div className={s.item}>
-        <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/profile">Profile</NavLink>
-      </div>
-      <div className={s.item}>
-        <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/dialogs">Dialogs</NavLink>
-      </div>
-      <div className={s.item}>
-        <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/news">News</NavLink>
-      </div>
-      <div className={s.item}>
-        <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/music">Music</NavLink>
-      </div>
-      <div className={s.item}>
-        <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/settings">Settings</NavLink>
-      </div>
-    </nav>
+    <>
+      <nav className={s.nav}>
+        <div className={s.item}>
+          <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/profile">Profile</NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/dialogs">Dialogs</NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/news">News</NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/music">Music</NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink className={({ isActive }) => isActive ? s.link_active : s.link} to="/settings">Settings</NavLink>
+        </div>
+      </nav>
+    <Friends />
+    </>
+
   );
 }
 
