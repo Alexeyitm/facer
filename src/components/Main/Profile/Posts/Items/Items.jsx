11f5b256decf2d2a24e481d78f8b505b66056d73
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-22 12:30:08 +0500

    ref: add map for posts

--- src/components/Main/Profile/Posts/Items/Items.jsx
+++ src/components/Main/Profile/Posts/Items/Items.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import s from './Items.module.css';
 import Item from './Item/Item';
 
+const posts = [
+  {id: 1, text: 'It was a good day!', likeCount: 3},
+  {id: 2, text: "What's happening?", likeCount: 6},
+  {id: 3, text: 'Yeeeaaaaaaaaaaa', likeCount: 11},
+  {id: 4, text: 'Hello, world!', likeCount: 14},
+]
+
 function Items() {
   return (
     <ul className={s.items}>
-      <Item text="It was a good day!" likes="3"/>
-      <Item text="What's happening?" likes="6"/>
-      <Item text="Yeeeaaaaaaaaaaa" likes="11"/>
-      <Item text="Hello, world!" likes="14"/>
+      {posts.map(post => <Item text={post.text} likes={post.likeCount} key={post.id}/>)}
     </ul>
   );
 }
