93eed302a8b6e7420c31cb6698310bc788124e32
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-22 09:48:44 +0500

    feat: add paths for dialogs

--- src/components/Main/Dialogs/Users/User/User.jsx
+++ src/components/Main/Dialogs/Users/User/User.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './User.module.css';
 
-function User({ name }) {
+function User({ name, id }) {
+  const path = '/dialogs/' + id;
+
   return (
-    <li className={s.user}><NavLink className={s.link} to="/">{name}</NavLink></li>
+    <li className={s.user}><NavLink className={s.link} to={path}>{name}</NavLink></li>
   );
 }
 
--- src/components/Main/Dialogs/Users/Users.jsx
+++ src/components/Main/Dialogs/Users/Users.jsx
@@ -5,18 +5,18 @@ import User from './User/User'
 function Users() {
   return (
     <ul className={s.users}>
-      <User name="Petya"/>
-      <User name="Ivan"/>
-      <User name="Kolya"/>
-      <User name="Dima"/>
-      <User name="Olya"/>
-      <User name="Artem"/>
-      <User name="Petya"/>
-      <User name="Ivan"/>
-      <User name="Kolya"/>
-      <User name="Dima"/>
-      <User name="Olya"/>
-      <User name="Artem"/>
+      <User name="Petya" id="1"/>
+      <User name="Ivan" id="2"/>
+      <User name="Kolya" id="3"/>
+      <User name="Dima" id="4"/>
+      <User name="Olya" id="5"/>
+      <User name="Artem" id="6"/>
+      <User name="Petya" id="7"/>
+      <User name="Ivan" id="8"/>
+      <User name="Kolya" id="9"/>
+      <User name="Dima" id="10"/>
+      <User name="Olya" id="11"/>
+      <User name="Artem" id="12"/>
     </ul>
   );
 }
