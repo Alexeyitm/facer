8fda6a6fc6eeaf2db3589852c4ad3259f3ad01a3
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-22 10:02:39 +0500

    ref: ref path

--- src/components/Main/Dialogs/Users/User/User.jsx
+++ src/components/Main/Dialogs/Users/User/User.jsx
@@ -3,10 +3,8 @@ import { NavLink } from 'react-router-dom';
 import s from './User.module.css';
 
 function User({ name, id }) {
-  const path = '/dialogs/' + id;
-
   return (
-    <li className={s.user}><NavLink className={s.link} to={path}>{name}</NavLink></li>
+    <li className={s.user}><NavLink className={s.link} to={id}>{name}</NavLink></li>
   );
 }
 
