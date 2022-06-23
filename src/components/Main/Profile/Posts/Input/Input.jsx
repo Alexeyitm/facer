3b31641d96b1e4df528d3c3422c2c2e5997c6586
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-23 12:34:14 +0500

    ref: move button component

--- src/components/Main/Profile/Posts/Input/Input.jsx
+++ src/components/Main/Profile/Posts/Input/Input.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from './Input.module.css';
+import Button from './Button/Button';
 
 function Input() {
   return (
-    <div>
+    <div className={s.input}>
       <h2 className={s.title}>My Posts</h2>
-      <input className={s.input}></input>
+      <textarea className={s.textarea}></textarea>
+      <Button />
     </div>
   );
 }
--- src/components/Main/Profile/Posts/Posts.jsx
+++ src/components/Main/Profile/Posts/Posts.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import s from './Posts.module.css';
 import Input from './Input/Input';
-import Button from './Button/Button';
 import Items from './Items/Items';
 
 function Posts({ avatar, posts }) {
   return (
     <div className={s.posts}>
       <Input />
-      <Button />
       <Items avatar={avatar} posts={posts}/>
     </div>
   );
