d7e12f425a8068d3876263a4db1f0da70a3213a8
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-23 13:47:12 +0500

    feat: add button

---
+++ src/components/Main/Dialogs/Input/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Button.module.css';
+
+function Button() {
+  return (
+    <button className={s.button}></button>
+  );
+}
+
+export default Button;
--- src/components/Main/Dialogs/Input/Input.jsx
+++ src/components/Main/Dialogs/Input/Input.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import s from './Input.module.css';
+import Button from './Button/Button'
 
 function Input() {
   return (
     <div className={s.input}>
       <textarea className={s.textarea}></textarea>
+      <Button />
     </div>
   );
 }
