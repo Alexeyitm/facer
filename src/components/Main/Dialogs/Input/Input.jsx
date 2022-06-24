ea119ef1802008a524abe504a0886a12a987c68e
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-06-24 09:53:43 +0500

    feat: create ref

--- src/components/Main/Dialogs/Input/Input.jsx
+++ src/components/Main/Dialogs/Input/Input.jsx
@@ -3,6 +3,9 @@ import s from './Input.module.css';
 import Button from './Button/Button'
 
 function Input() {
+
+  const input = React.createRef();
+  
   return (
     <div className={s.input}>
       <textarea className={s.textarea}></textarea>
